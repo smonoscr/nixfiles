30f7a0bd32afd50738f5812fef8af851cac556a9
simonoscr <117449098+simonoscr@users.noreply.github.com>
2024-02-29 18:45:54 +0100

    fix: pipewire config anf update flock

--- hosts/cosmos/audio.nix
+++ hosts/cosmos/audio.nix
@@ -17,11 +17,43 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
-    lowLatency = {
-      enable = true;
-      quantum = 256;
-      rate = 44100;
+    #lowLatency = {
+    #  enable = true;
+    #  quantum = qr;
+    #  rate = re;
+    #};
+    extraConfig.pipewire = {
+      "92-low-latency" = {
+        "context.properties" = {
+          "default.clock.rate" = 44100;
+          "default.clock.allowed-rates" = [44100];
+          "default.clock.quantum" = 128;
+          "default.clock.min-quantum" = 128;
+          "default.clock.max-quantum" = 128;
+        };
+      };
     };
+    #extraConfig.pipewire-pulse = {
+    #  "99-low-latency" = {
+    #    "context.modules" = [
+    #      {
+    #        "name" = "libpipewire-module-protocol-pulse";
+    #        "args" = {
+    #          "server.address" = ["unix:native"];
+    #          "pulse.min.req" = "128/44100";
+    #          "pulse.default.req" = "128/44100";
+    #          "pulse.max.req" = "128/44100";
+    #          "pulse.min.quantum" = "128/44100";
+    #          "pulse.max.quantum" = "128/44100";
+    #        };
+    #      }
+    #    ];
+    #    "stream.properties" = {
+    #      "node.latency" = "128/44100";
+    #      "resample.quality" = 1;
+    #    };
+    #  };
+    #};
   };
 
   environment.systemPackages = with pkgs; [
