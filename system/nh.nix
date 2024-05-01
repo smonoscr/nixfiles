15d3d8d9adcc75b01fda66e026cd4c92ec3a543d
simonoscr <117449098+simonoscr@users.noreply.github.com>
2024-05-01 17:58:22 +0200

    ci: add statix to pipeline

--- system/nh.nix
+++ system/nh.nix
@@ -4,7 +4,7 @@ _: {
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    clean.extraArgs = "--keep 3";
     flake = "/home/simon/nixfiles";
   };
 }
