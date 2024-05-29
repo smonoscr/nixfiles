e03d74ae276722ec4941ca5b4858e10c7e453e72
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-05-29 19:07:15 +0200

    fix(hyprland): windowrules

--- home/desktop/wayland/hyprland/rules.nix
+++ home/desktop/wayland/hyprland/rules.nix
@@ -3,29 +3,17 @@ _: {
     settings = {
       windowrulev2 = [
         # webcord
-        "float, class:^(WebCord)$"
         "workspace 2, class:^(WebCord)$"
-        "size 1200 800, class:^(WebCord)$"
-        "center, class:^(WebCord)$"
 
         # steam
-        "float, class:^(steam)$"
         "workspace 2, class:^(steam)$"
-        "center, class:^(steam)$"
-
-        "size 1200 800, title:^(Steam)$"
-        "size 300 1000, title:^(Friends List)$"
-        "size 800 800, title:^(Steam Settings)$"
 
         # ts3
-        "float, title:^(TeamSpeak 3)$"
         "workspace 2, title:^(TeamSpeak 3)$"
-        "size 450 600, title:^(TeamSpeak 3)$"
-        "center, title:^(TeamSpeak 3)$"
 
         # suppress notification events when gaming
         "suppressevent maximize,class:.*"
-        "noinitialfocus,title:(^notificationtoasts.*)"
+        "noinitialfocus, title:^(notificationtoasts.*)$, floating:1"
 
         # idle inhibit on any class that is fullscreen
         "idleinhibit fullscreen, class:.*"
