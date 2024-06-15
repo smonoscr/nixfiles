2e53a3473170f30ec3716a5142a774044e107e05
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-06-15 02:27:10 +0200

    fix: update flake and set correct FLAKE url for nh

--- system/nix/nh.nix
+++ system/nix/nh.nix
@@ -1,9 +1,9 @@
 _: {
-  environment.variables.FLAKE = "/home/simon/nixfiles";
+  environment.variables.FLAKE = "/home/simon/code/nixfiles";
 
   programs.nh = {
     enable = true;
-    flake = "/home/simon/nixfiles";
+    flake = "/home/simon/code/nixfiles";
     clean = {
       enable = true;
       extraArgs = "--keep 3";
