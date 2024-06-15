1daf16429b910e6c8eb5739eb349da580026c2cd
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-06-15 14:21:00 +0200

    fix: add sops-nix config file for profile w

--- home/work/secrets/sops.nix
+++ home/work/secrets/sops.nix
@@ -1,1 +1,21 @@
-_: {}
+{config, ...}: {
+  sops = {
+    validateSopsFiles = false;
+    defaultSopsFile = "${config.home.homeDirectory}/code/nixsecrets/secrets/work/secrets.yaml";
+    age = {
+      sshKeyPaths = ["${config.home.homeDirectory}/.ssh/id_rsa"];
+      keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+      generateKey = false;
+    };
+    secrets = {
+      d29yay1yc2EucHVi = {
+        path = "${config.home.homeDirectory}/.ssh/id_rsa.pub";
+        mode = "0600";
+      };
+      d29yay1yc2E = {
+        path = "${config.home.homeDirectory}/.ssh/id_rsa";
+        mode = "0600";
+      };
+    };
+  };
+}
