619cd1c8e2c01a713d28df4982605b2b9a247e96
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-06-17 18:13:55 +0200

    fix:(pre-commit-hook): more dynamic pre-commit-hook

--- pre-commit-hooks.nix
+++ pre-commit-hooks.nix
@@ -1,96 +1,91 @@
-{ inputs, pkgs, ... }:
+{ inputs, ... }:
 {
   imports = [ inputs.pre-commit-hooks.flakeModule ];
 
-  perSystem.pre-commit.settings = {
-    excludes = [
-      "flake.lock"
-      "CHANGELOG.md"
-      "LICENSE"
-    ];
-    src = ./.;
-    hooks = {
-      #alejandra = {
-      #  enable = true;
-      #  fail_fast = true;
-      #  settings = {
-      #    check = false;
-      #  };
-      #};
-      nixfmt = {
-        enable = true;
-        package = pkgs.nixfmt-rfc-style;
-      };
-
-      statix = {
-        enable = true;
-        fail_fast = true;
-      };
-
-      deadnix = {
-        enable = true;
-        fail_fast = true;
-        settings = {
-          edit = true;
+  perSystem.pre-commit = {
+    check.enable = true;
+    settings = {
+      excludes = [
+        "flake.lock"
+        "CHANGELOG.md"
+        "LICENSE"
+      ];
+      src = ./.;
+      hooks = {
+        nixfmt = {
+          enable = true;
         };
-      };
 
-      yamllint = {
-        enable = true;
-        fail_fast = true;
-        settings = {
-          format = "colored";
-          preset = "relaxed";
-          configuration = ''
-            ---
-
-            extends: relaxed
-
-            rules:
-              braces:
-                level: warning
-                max-spaces-inside: 1
-              brackets:
-                level: warning
-                max-spaces-inside: 1
-              colons:
-                level: warning
-              commas:
-                level: warning
-              comments: disable
-              comments-indentation: disable
-              document-start: disable
-              empty-lines:
-                level: warning
-              hyphens:
-                level: warning
-              indentation:
-                level: warning
-                indent-sequences: consistent
-              line-length: disable
-              truthy: disable
-          '';
+        statix = {
+          enable = true;
+          fail_fast = true;
         };
+
+        deadnix = {
+          enable = true;
+          fail_fast = true;
+          settings = {
+            edit = true;
+          };
+        };
+
+        yamllint = {
+          enable = true;
+          fail_fast = true;
+          settings = {
+            format = "colored";
+            preset = "relaxed";
+            configuration = ''
+              ---
+
+              extends: relaxed
+
+              rules:
+                braces:
+                  level: warning
+                  max-spaces-inside: 1
+                brackets:
+                  level: warning
+                  max-spaces-inside: 1
+                colons:
+                  level: warning
+                commas:
+                  level: warning
+                comments: disable
+                comments-indentation: disable
+                document-start: disable
+                empty-lines:
+                  level: warning
+                hyphens:
+                  level: warning
+                indentation:
+                  level: warning
+                  indent-sequences: consistent
+                line-length: disable
+                truthy: disable
+            '';
+          };
+        };
+
+        #typos = {
+        #  enable = true;
+        #  settings = {
+        #    write = true;
+        #    configPath = "./.typos.toml";
+        #  };
+        #};
+
+        #prettier = {
+        #  enable = true;
+        #  fail_fast = true;
+        #  excludes = [".js" ".md" ".ts" ".scss" ".css"];
+        #  settings = {
+        #    write = true;
+        #  };
+        #};
+
+        pre-commit-hook-ensure-sops.enable = true;
       };
-
-      #typos = {
-      #  enable = true;
-      #  settings = {
-      #    write = true;
-      #    configPath = "./.typos.toml";
-      #  };
-      #};
-
-      #prettier = {
-      #  enable = true;
-      #  fail_fast = true;
-      #  excludes = [".js" ".md" ".ts" ".scss" ".css"];
-      #  settings = {
-      #    write = true;
-      #  };
-      #};
-
-      pre-commit-hook-ensure-sops.enable = true;
     };
   };
 }
