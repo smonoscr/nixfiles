03c7d1af4755be416449db2b43c3255d7c14d6aa
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-07-06 19:52:38 +0200

    fix(gaming): steam without extrapkgs and gamescope with capsysnice enabled

--- hosts/desktop/gaming/gamescope.nix
+++ hosts/desktop/gaming/gamescope.nix
@@ -1,16 +1,19 @@
 { config, ... }:
 {
   programs.gamescope = {
-    enable = false;
-    capSysNice = false; # https://discourse.nixos.org/t/unable-to-activate-gamescope-capsysnice-option/37843
-    args = [ "--rt" ];
+    enable = true;
+    capSysNice = true; # FIXME https://discourse.nixos.org/t/unable-to-activate-gamescope-capsysnice-option/37843
+    args = [
+      "--rt"
+      "--expose-wayland"
+    ];
   };
 
   # doesn't work at the moment, so caps not set, but doesn't hurt to keep this here
-  security.wrappers.gamescope = {
-    owner = "root";
-    group = "root";
-    source = "${config.programs.gamescope.package}/bin/gamescope";
-    capabilities = "cap_sys_nice+pie";
-  };
+  #security.wrappers.gamescope = {
+  #  owner = "root";
+  #  group = "root";
+  #  source = "${config.programs.gamescope.package}/bin/gamescope";
+  #  capabilities = "cap_sys_nice+pie";
+  #};
 }
--- hosts/desktop/gaming/steam.nix
+++ hosts/desktop/gaming/steam.nix
@@ -3,23 +3,25 @@
   programs.steam = {
     enable = true;
 
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
+
+    gamescopeSession.enable = true;
 
     # fix gamescope inside steam
-    package = pkgs.steam.override {
-      extraPkgs =
-        pkgs: with pkgs; [
-          keyutils
-          libkrb5
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-        ];
-    };
+    #package = pkgs.steam.override {
+    #  extraPkgs =
+    #    pkgs: with pkgs; [
+    #      keyutils
+    #      libkrb5
+    #      libpng
+    #      libpulseaudio
+    #      libvorbis
+    #      stdenv.cc.cc.lib
+    #      xorg.libXcursor
+    #      xorg.libXi
+    #      xorg.libXinerama
+    #      xorg.libXScrnSaver
+    #    ];
+    #};
   };
 }
