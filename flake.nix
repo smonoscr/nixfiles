9f7156702e7cfa3487b816d879f23894539ee1ef
smonoscr <smonoscr@noreply.codeberg.org>
2024-07-10 08:27:39 +0200

    chore(flake): ommit extra- prefix for nixConfig substituters and public key

--- flake.nix
+++ flake.nix
@@ -2,20 +2,20 @@
   description = "simonoscr's flake for nixos and home-manager";
 
   nixConfig = {
-    extra-experimental-features = "nix-command flakes";
-    extra-trusted-substituters = [
+    experimental-features = "nix-command flakes";
+    trusted-substituters = [
       "https://cache.nixos.org?priority=10"
-      "https://yazi.cachix.org"
-      "https://nix-community.cachix.org"
+      "https://nix-community.cachix.org?priority=20"
+      "https://yazi.cachix.org?priority=25"
       "https://hyprland.cachix.org1"
       "https://simonoscr.cachix.org"
     ];
-    extra-trusted-public-keys = [
+    trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "simonoscr.cachix.org-1:pTFtYU3a9SXL+Fw6S3sZ8uk+Vd33Yoothd771oGNJBE="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "simonoscr.cachix.org-1:pTFtYU3a9SXL+Fw6S3sZ8uk+Vd33Yoothd771oGNJBE="
     ];
   };
 
