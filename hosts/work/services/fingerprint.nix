f222529c3ebecfa3b5993ec2c11221d1435c8655
smonoscr <smonoscr@noreply.codeberg.org>
2024-07-13 13:29:13 +0200

    fix: enable fingerprint and secureboot

--- hosts/work/services/fingerprint.nix
+++ hosts/work/services/fingerprint.nix
@@ -5,10 +5,13 @@
     serviceConfig.Type = "simple";
   };
 
-  services.fprintd.enable = true;
-  services.fprintd.tod.enable = true;
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
-  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-elan # Elan(04f3:0c4b) driver
-  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090; # driver for 2016 ThinkPads
-  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a # Goodix 550a driver (from Lenovo)
+  services = {
+    fprintd = {
+      enable = true;
+      #tod = {
+      #  enable = true;
+      #  driver = pkgs.libfprint-2-tod1-goodix;
+      #};
+    };
+  };
 }
