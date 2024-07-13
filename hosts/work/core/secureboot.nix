0adcd4787aaa0d79b895100d8e240666853b15fc
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-07-13 11:34:09 +0200

    fix(work): disable secureboot for initial setup

--- hosts/work/core/secureboot.nix
+++ hosts/work/core/secureboot.nix
@@ -7,8 +7,6 @@
 {
   imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
 
-  environment.systemPackages = with pkgs; [ sbctl ];
-
   boot = {
     loader.systemd-boot.enable = lib.mkForce false;
     lanzaboote = {
