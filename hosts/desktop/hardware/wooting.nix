4bd6ee83eecc5380cc6b07b02fa593d4a859aea7
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-07-16 20:10:10 +0200

    fix: udev rules for wooting

--- hosts/desktop/hardware/wooting.nix
+++ hosts/desktop/hardware/wooting.nix
@@ -1,1 +1,24 @@
-_: { hardware.wooting.enable = true; }
+_: {
+  hardware.wooting.enable = true;
+  services = {
+    udev.extraRules = ''
+      # Wooting One Legacy
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
+
+      # Wooting One update mode
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2402", TAG+="uaccess"
+
+      # Wooting Two Legacy
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
+
+      # Wooting Two update mode
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2403", TAG+="uaccess"
+
+      # Generic Wootings
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="31e3", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="31e3", TAG+="uaccess"
+    '';
+  };
+}
