8dc66d9b823b92cfc46915a77720f1cd112a5754
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-07-19 21:19:17 +0200

    chore(flake): update flake

--- hosts/server/pkgs/packages.nix
+++ hosts/server/pkgs/packages.nix
@@ -1,15 +1,18 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    git
-    #neofetch
-    neovim
-    btop
-    bat
-    curl
-    dig
-    # hypervisor
-    qemu
-    k9s
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      git
+      #neofetch
+      neovim
+      btop
+      bat
+      curl
+      dig
+      # hypervisor
+      qemu
+      k9s
+    ];
+    defaultPackages = lib.mkForce [ ]; # no extra default packages are installed
+  };
 }
