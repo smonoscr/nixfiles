5c2972617a5d20fd79ce1ee9a3116a3fa16e5622
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-07-20 21:21:30 +0200

    fix(yubikey): drop unused packages and enable u2f

--- hosts/desktop/services/yubikey.nix
+++ hosts/desktop/services/yubikey.nix
@@ -21,18 +21,17 @@
     #  control = "sufficient";
     #  interactive = true;
     #};
-    yubico = {
-      enable = true;
-      debug = true;
-      mode = "challenge-response";
-      id = [ "29507896" ];
-    };
-    #services = {
-    #  login.u2fAuth = true;
-    #  greetd.u2fAuth = true;
-    #  sudo.u2fAuth = true;
-    #  hyprlock.u2fAuth = true;
+    #yubico = {
+    #  enable = true;
+    #  debug = true;
+    #  mode = "challenge-response";
+    #  id = [ "29507896" ];
     #};
+    services = {
+      login.u2fAuth = true;
+      greetd.u2fAuth = true;
+      hyprlock.u2fAuth = true;
+    };
   };
 
   #users.users.yubikey = {
@@ -46,10 +45,6 @@
     yubikey-manager-qt
     yubikey-personalization
     yubikey-personalization-gui
-    yubico-piv-tool
-    yubioath-flutter
     age-plugin-yubikey
-    #pam_u2f
-    yubico-pam
   ];
 }
