8585a0576695d674b2e911ff30d512438e0a9cc8
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-07-23 21:47:58 +0200

    fix: hyprgamemode

--- home/simon/desktop/wayland/hyprland/hyprmode.nix
+++ home/simon/desktop/wayland/hyprland/hyprmode.nix
@@ -13,11 +13,13 @@ let
       hyprctl keyword decoration:rounding 0
       hyprctl keyword monitor "DP-1,2560x1440@143.97,auto,1" 2> /dev/null
       pkill -f ags
+      powerprofilesctl set performance
       exit
     fi
     hyprctl keyword monitor "DP-1,3440x1440@164.90,auto,1" 2> /dev/null
     hyprctl reload
     hyprctl dispatch -- exec "ags -b hypr" --single-instance
+    powerprofilesctl set balanced
   '';
 in
 {
