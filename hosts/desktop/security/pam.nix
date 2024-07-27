ed4dfb0e44d18d721693462ffd0c134f886ec72c
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-07-27 13:08:20 +0200

    fix(pam): add pams

--- hosts/desktop/security/pam.nix
+++ hosts/desktop/security/pam.nix
@@ -2,8 +2,22 @@
 {
   security.pam = {
     services = {
-      hyprlock.text = "auth include login";
+      hyprlock = {
+        text = "auth include login";
+        enableGnomeKeyring = true;
+      };
       ags = { };
+      login = {
+        enableGnomeKeyring = true;
+      };
+
+      greetd = {
+        enableGnomeKeyring = true;
+      };
+
+      tuigreet = {
+        enableGnomeKeyring = true;
+      };
     };
   };
 }
