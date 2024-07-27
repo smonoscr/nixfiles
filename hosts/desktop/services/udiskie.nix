c1dd72653c5875ee0ab7b54528bd3364292a1edc
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-07-27 13:07:23 +0200

    fix(udiskie): storage daemon for system

---
+++ hosts/desktop/services/udiskie.nix
@@ -0,0 +1,24 @@
+{ config, ... }:
+{
+  config = {
+    services = {
+      # storage daemon required for udiskie auto-mount
+      udisks2 = {
+        enable = true;
+        settings = {
+          # general udisks2 configuration
+          "udisks2.conf" = {
+            defaults = {
+              encryption = "luks2"; # alternatively, luks1
+            };
+
+            udisks2 = {
+              modules = [ "*" ];
+              modules_load_preference = "ondemand";
+            };
+          };
+        };
+      };
+    };
+  };
+}
