e4799ea2917136383a13f8ec9943992d00d7b2bd
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-07-27 13:10:05 +0200

    feat(systemd): add systemd config file and disable ttys and set default timeouts

---
+++ hosts/desktop/services/systemd.nix
@@ -0,0 +1,30 @@
+{ lib, ... }:
+{
+  systemd =
+    let
+      timeoutConfig = ''
+        DefaultTimeoutStartSec=10s
+        DefaultTimeoutStopSec=10s
+        DefaultTimeoutAbortSec=10s
+        DefaultDeviceTimeoutSec=10s
+      '';
+    in
+    {
+      # Set the default timeout for starting, stopping, and aborting services to
+      # avoid hanging the system for too long on boot or shutdown.
+      extraConfig = timeoutConfig;
+      user.extraConfig = timeoutConfig;
+
+      # Disable (not) all virtual terminals. I usually don't need to switch between
+      # TTYs, however, I may get locked out of my desktop session and this
+      # may come to bite me in the ass when that happens.
+      services = lib.mapAttrs (_: lib.mkForce) {
+        "getty@tty1".enable = false;
+        "autovt@tty1".enable = true;
+        "getty@tty7".enable = false;
+        "autovt@tty7".enable = false;
+        "kmsconvt@tty1".enable = false;
+        "kmsconvt@tty7".enable = false;
+      };
+    };
+}
