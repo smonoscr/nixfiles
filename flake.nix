b8dd00c74425b6784c904e4d6b93faa8302de698
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-08-03 16:10:45 +0200

    chore(flake): added chaotic nyx input

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
