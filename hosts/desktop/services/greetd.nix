91e029300468e87d44ef9cd469fbbda6183fc729
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-09-07 16:20:37 +0200

    fix(greetd): remove false StandardInput

--- hosts/desktop/services/greetd.nix
+++ hosts/desktop/services/greetd.nix
@@ -39,7 +39,7 @@ in
   # See: https://github.com/apognu/tuigreet/issues/68#issuecomment-1586359960
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
-    StandardInputs = "tty";
+    #StandardInputs = "tty";
     StandardOutput = "tty";
     StandardError = "journal";
     TTYReset = true;
