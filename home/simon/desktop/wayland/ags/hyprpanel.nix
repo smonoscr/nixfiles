dde55285d70d27fdf7cb2398ac5a83c0884e9916
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-09-11 20:03:50 +0200

    fix(hyprpanel): update

--- home/simon/desktop/wayland/ags/hyprpanel.nix
+++ home/simon/desktop/wayland/ags/hyprpanel.nix
@@ -1,8 +1,18 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [ inputs.ags.homeManagerModules.ags ];
 
+  home.packages = with pkgs; [
+    brightnessctl
+    bun
+    dart-sass
+    hyprpanel
+    swww
+
+  ];
+
   programs.ags = {
     enable = true;
+    systemd.enable = true;
   };
 }
