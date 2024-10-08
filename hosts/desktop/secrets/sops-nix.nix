5e2de8ee681c53147d8bef5b959ecfddfc4b55ff
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-10-08 20:28:45 +0200

    feat: restructure secrets and prepare agenix

--- hosts/desktop/secrets/sops-nix.nix
+++ hosts/desktop/secrets/sops-nix.nix
@@ -1,7 +1,10 @@
-{ inputs, config, ... }:
+{
+  inputs,
+  config,
+  ...
+}:
 let
-  secretsDirectory = builtins.toString inputs.nixsecrets;
-  secretsFile = "${secretsDirectory}/secrets/simon/secrets.yaml";
+  secretsFile = "${inputs.nixsecrets}/sops-nix/simon/secrets.yaml";
 in
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
@@ -17,7 +20,7 @@ in
       # i use ssh key ed25519 so i can generate age key directly from it. so i neither need to reference an age key nor do i need to backup my age key.
       # this is very convinient
       keyFile = "${config.users.users.simon.home}/.config/sops/age/keys.txt";
-      generateKey = true;
+      generateKey = false;
     };
 
     # disable importing host ssh keys
@@ -35,24 +38,12 @@ in
         owner = "simon";
         mode = "0600";
       };
-      "user/password" = {
-        neededForUsers = true;
-      };
-      "user/pin" = {
-        neededForUsers = true;
-      };
-
       "yubikey/u2f_keys" = { };
 
-      "git/github/github-pat" = { };
-      "git/github/fg-github-pat" = { };
-
-      "git/gitlab/gitlab-pat" = { };
-    };
-    templates = {
-      "nix-access-tokens".content = ''access-tokens = github.com=${
-        config.sops.placeholder."git/github/github-pat"
-      }'';
+      "nix/access-tokens/github" = {
+        mode = "0440";
+        group = config.users.groups.keys.name;
+      };
     };
   };
 }
