2005c819ca16ac694bea7286cb252f0344a2a8d8
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-10-08 23:37:06 +0200

    chore(flake): update flake

--- hosts/desktop/programs/hyprland.nix
+++ hosts/desktop/programs/hyprland.nix
@@ -5,6 +5,7 @@
   # enable hyprland and required options
   programs.hyprland = {
     enable = true;
+    xwayland.enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage =
       inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
