8c45dce671dcf136b3bbbbcbccc692b33ac4af84
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-10-18 20:46:40 +0200

    feat(overlays): add nixpkgs stable, git and unstable inputs as overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,19 @@
-{ ... }:
 {
-  imports = [
-    ./hyprpanel.nix
-  ];
+  self,
+  lib,
+  ...
+}:
+
+let
+  overlaysPath = "${self}/overlays/nixpkgs";
+in
+{
+  # Read all directories from systemModules
+  imports =
+    [
+      ./hyprpanel.nix
+    ]
+    ++ builtins.filter (module: lib.pathIsDirectory module) (
+      map (module: "${overlaysPath}/${module}") (builtins.attrNames (builtins.readDir overlaysPath))
+    );
 }
