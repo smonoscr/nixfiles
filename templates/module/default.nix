ee2724e6cfdde173e6c1b3c3d8b9ded2d430b4a9
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-10-18 20:40:17 +0200

    feat: add templates

---
+++ templates/module/default.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib;
+
+let
+  cfg = config.module;
+in
+{
+  options.module = {
+    enable = mkEnableOption "Enable module";
+  };
+
+  config =
+    mkIf cfg.enable
+      {
+      };
+}
