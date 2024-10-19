2dde1bf1e7f046545b5dbe37de2d2f965715ad08
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-10-19 09:03:02 +0200

    fix(zed-editor): use hm module

--- home/simon/editors/zed/default.nix
+++ home/simon/editors/zed/default.nix
@@ -4,8 +4,22 @@
     ./settings.nix
   ];
 
+  programs.zed-editor = {
+    enable = true;
+    extensions = [
+      "nix"
+      "html"
+      "dockerfile"
+      "toml"
+      "git-firefly"
+      "xml"
+      "vscode-dark-modern"
+      "java"
+      "docker-compose"
+    ];
+  };
+
   home.packages = with pkgs; [
-    zed-editor
     nixd
   ];
 }
