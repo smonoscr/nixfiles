7c68105a532d5b6678a907f5b0fc3050b72a561d
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-10-31 19:32:18 +0100

    fix: make it possible to chose bars witrh module definition

--- home/simon/desktop/wayland/bars/default.nix
+++ home/simon/desktop/wayland/bars/default.nix
@@ -1,5 +1,21 @@
-_: {
+{ lib, ... }:
+
+with lib;
+{
+
+  options = {
+    module.desktop.wayland.bar = mkOption {
+      type = types.enum [
+        "hyprpanel"
+        "ags"
+      ];
+      default = "hyprpanel";
+      description = "Selects which bar to enable: either 'hyprpanel' or 'ags'";
+    };
+  };
+
   imports = [
     ./ags
+    ./hyprpanel
   ];
 }
