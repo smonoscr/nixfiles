b331ce0a057ad87350e869c081fc1f9a419f2337
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-12-01 16:45:03 +0100

    fix(hayprland): enable withUWSM key

--- hosts/desktop/programs/hyprland.nix
+++ hosts/desktop/programs/hyprland.nix
@@ -5,6 +5,7 @@
   # enable hyprland and required options
   programs.hyprland = {
     enable = true;
+    withUWSM = true;
     xwayland.enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage =
