2a5ea42f011a5742059612c8b376762bb143d32c
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-12-01 16:06:30 +0100

    fix(hyprpanel): add systemd services with uwsm

--- home/simon/desktop/wayland/bars/hyprpanel/default.nix
+++ home/simon/desktop/wayland/bars/hyprpanel/default.nix
@@ -15,25 +15,29 @@ in
     home.packages = with pkgs; [
       hyprpanel
     ];
-    #systemd.user.services.hyprpanel = {
-    #  Unit = {
-    #    Description = "HyprPanel - A Bar/Panel built with AGS for Hyprland.";
-    #    Documentation = "https://github.com/Jas-SinghFSU/HyprPanel";
-    #    PartOf = [
-    #      "tray.target"
-    #      "graphical-session.target"
-    #    ];
-    #    After = "graphical-session.target";
-    #  };
+    systemd.user = {
+      services.hyprpanel = {
+        Unit = {
+          Description = "HyprPanel - A Bar/Panel built with AGS for Hyprland.";
+          Documentation = "https://github.com/Jas-SinghFSU/HyprPanel";
+          PartOf = [
+            "tray.target"
+            "graphical-session.target"
+          ];
+          Before = "tray.target";
+          After = "graphical-session.target";
+        };
 
-    #  Service = {
-    #    ExecStart = "${pkgs.hyprpanel}/bin/hyprpanel";
-    #    Restart = "on-failure";
-    #  };
+        Service = {
+          ExecStart = "${pkgs.hyprpanel}/bin/hyprpanel";
+          Restart = "on-failure";
+        };
 
-    #  Install = {
-    #    WantedBy = [ "graphical-session.target" ];
-    #  };
-    #};
+        Install = {
+          WantedBy = [ "graphical-session.target" ];
+        };
+      };
+      targets.tray.Unit.After = lib.mkForce [ "hyprpanel.service" ];
+    };
   };
 }
