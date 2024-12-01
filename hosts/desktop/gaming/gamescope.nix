07588be3b4f35a85e3c349a47cb5c87aca2cbd20
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-12-01 16:41:44 +0100

    fix(gmaing): remove extraPkgs, setup gamescope and add tweaks

--- hosts/desktop/gaming/gamescope.nix
+++ hosts/desktop/gaming/gamescope.nix
@@ -1,6 +1,8 @@
-_: {
+{ pkgs, ... }:
+{
   programs.gamescope = {
     enable = true;
+    package = pkgs.gamescope_git;
     capSysNice = false; # FIXME https://discourse.nixos.org/t/unable-to-activate-gamescope-capsysnice-option/37843
     args =
       [
