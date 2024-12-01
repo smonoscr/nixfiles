a11e79fe9e33aae203c66ec3460a219974d11c28
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2024-12-01 16:05:11 +0100

    fix: add systemd services for diff services

--- home/simon/services/tailray.nix
+++ home/simon/services/tailray.nix
@@ -22,5 +22,8 @@ in
 
     # tailscale systray
     services.tailray.enable = true;
+
+    systemd.user.services.tailray.Unit.After = lib.mkForce "graphical-session.target";
   };
+
 }
--- home/simon/services/udiskie.nix
+++ home/simon/services/udiskie.nix
@@ -20,5 +20,6 @@ in
       notify = true;
       tray = "auto";
     };
+    systemd.user.services.udiskie.Unit.After = lib.mkForce "graphical-session.target";
   };
 }
