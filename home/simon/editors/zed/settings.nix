33aacc30733ebf2d1f68f6a2aa4907dad57e52ca
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2025-01-31 19:14:03 +0100

    fix(zeditor): update zed settings and config

--- home/simon/editors/zed/settings.nix
+++ home/simon/editors/zed/settings.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
+let
+  inherit (pkgs.zed-editor) version remote_server;
+  binary_name = "zed-remote-server-stable-${version}";
+in
 {
+  #  you may want to consider using the nixpkgs provided server instead if you have custom patches to apply or mistrust whatever binary upstream Zed is downloading to your server
+  # use this magic to only symlink the binary and not the directory, so Zed will successfully fall back to its own binary upload if the remote package version does not match
+  home.file.".zed_server/${binary_name}".source = "${remote_server}/bin/${binary_name}";
+
   programs.zed-editor = {
     extensions = [
       "dockerfile"
@@ -37,30 +45,27 @@
       indent_guides = {
         coloring = "indent_aware";
       };
-      search = {
-        regex = true;
-      };
       inlay_hints = {
         enabled = true;
       };
       project_panel = {
         indent_size = 15;
         auto_fold_dirs = false;
-        scrollbar = {
-          show = "auto";
-        };
       };
       outline_panel = {
         indent_size = 15;
         auto_fold_dirs = false;
       };
-      #assistant = {
-      #  version = "2";
-      #  default_model = {
-      #    provider = "ollama";
-      #    model = "deepseek-coder-v2:16b";
-      #  };
-      #};
+      collaboration_panel = {
+        button = false;
+      };
+      git_panel = {
+        button = true;
+      };
+      assistant = {
+        enabled = false;
+        button = false;
+      };
       #language_models = {
       #  ollama = {
       #    api_url = "http://localhost:11434";
@@ -72,12 +77,12 @@
           milliseconds = 2000;
         };
       };
-      tab_bar = {
-        show_nav_history_buttons = false;
-      };
       tabs = {
         git_status = true;
         file_icons = true;
+        always_show_close_button = true;
+        activate_on_close = "neighbour";
+        show_diagnostics = "all";
       };
       extend_comment_on_newline = false;
       tab_size = 2;
@@ -95,6 +100,7 @@
         "**/.git"
         "**/.svn"
         "**/.hg"
+        "**/.jj"
         "**/CVS"
         "**/.DS_Store"
         "**/Thumbs.db"
@@ -104,9 +110,8 @@
       journal = {
         hour_format = "hour24";
       };
-
+      load_direnv = "shell_hook";
       terminal = {
-        copy_on_select = false;
         env = {
           EDITOR = "zed --wait";
         };
@@ -120,23 +125,11 @@
           "**/.zed/**/*.json"
           "**/zed/**/*.json"
           "**/Zed/**/*.json"
-          "tsconfig.json"
-          "pyrightconfig.json"
+          "**/.vscode/**/*.json"
         ];
+        "Shell Script" = [ ".env.*" ];
         Dockerfile = [ "Dockerfile*" ];
         TOML = [ "uv.lock" ];
-      };
-      auto_install_extensions = {
-        html = true;
-        nix = true;
-        dockerfile = true;
-        toml = true;
-        git-firefly = true;
-        xml = true;
-        vscode-dark-modern = true;
-        java = true;
-        helm = true;
-        ansible = true;
       };
       languages = {
         Nix = {
@@ -160,15 +153,26 @@
         };
         Markdown = {
           format_on_save = "on";
-          use_on_type_format = false;
           remove_trailing_whitespace_on_save = false; # If you rely on invisible trailing whitespace being converted to <br />
-          prettier = {
-            allowed = true;
-          };
+        };
+        YAML = {
+          # this fixes worng error for multiple manifest documents in a single .yaml file. docker-compose extensions fault
+          language_servers = [
+            "yaml-language-server"
+            "!docker-compose"
+          ];
         };
       };
       lsp = {
+        nix = {
+          binary = {
+            path_lookup = true;
+          };
+        };
         nixd = {
+          binary = {
+            path_lookup = true;
+          };
           settings = {
             diagnostics = {
               suppress = [ "sema-extra-with" ];
@@ -176,6 +180,9 @@
           };
         };
         nil = {
+          binary = {
+            path_lookup = true;
+          };
           settings = {
             diagnostics = {
               ignored = [ "unused_binding" ];
