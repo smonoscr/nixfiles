9aad0885fbab9abc9f4028d755342a2d09cf8da2
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2025-01-31 18:23:16 +0100

    fix(hyprland): nixos module pkgs

--- hosts/desktop/programs/hyprland.nix
+++ hosts/desktop/programs/hyprland.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ pkgs, inputs, ... }:
 {
   imports = [ inputs.hyprland.nixosModules.default ];
 
@@ -6,6 +6,9 @@
 
   programs.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     withUWSM = true;
   };
 }
