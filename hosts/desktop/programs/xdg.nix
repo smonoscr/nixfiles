acef413544d5256392d33de34a21e042ea371cb0
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2025-02-21 19:30:37 +0100

    fix: XDG portals fixing not doing xdg-open

--- hosts/desktop/programs/xdg.nix
+++ hosts/desktop/programs/xdg.nix
@@ -5,11 +5,16 @@
     xdgOpenUsePortal = true;
     config = {
       common.default = [ "gtk" ];
-      hyprland.default = [
-        "gtk"
-        "hyprland"
-      ];
+      hyprland = {
+        default = [
+          "gtk"
+          "hyprland"
+        ];
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+      };
     };
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
   };
 }
