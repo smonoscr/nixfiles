87bab513f8c5d520eb20d7c5c52e062aab99dff9
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2025-03-01 09:46:32 +0100

    fix(gaming): update sc, and config little helper

--- hosts/desktop/gaming/gamemode.nix
+++ hosts/desktop/gaming/gamemode.nix
@@ -1,12 +1,17 @@
-_: {
+{ pkgs, ... }:
+{
   programs.gamemode = {
     enable = true;
     enableRenice = true;
     settings = {
       general = {
         softrealtime = "auto";
-        renice = 15;
+        renice = 10;
         inhibit_screensaver = 0; # dont use screensaver anyway
+      };
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
       };
     };
   };
