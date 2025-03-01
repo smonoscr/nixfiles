5b95338e45f87bd79a2ed196b87422fceb83b32e
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2025-03-01 09:45:32 +0100

    fix(hyprland): update settings and remove unused plugins

--- home/simon/desktop/wayland/hyprland/plugins.nix
+++ home/simon/desktop/wayland/hyprland/plugins.nix
@@ -20,35 +20,15 @@ in
     wayland.windowManager.hyprland = {
       plugins = with inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}; [
         csgo-vulkan-fix
-        #hy3
-        #hyprbars
-        #hyprexpo
       ];
 
       settings = {
         plugin = {
           csgo-vulkan-fix = {
-            res_w = 1440;
-            res_h = 1080;
+            res_w = 1680;
+            res_h = 1050;
             class = "cs2";
-            fix_mouse = true;
-          };
-
-          hyprbars = {
-            bar_height = 25;
-            bar_precedence_over_border = false;
-            bar_part_of_window = true;
-
-            # r to l
-            hyprbars-button = [
-              "rgb(ff4040), 14, 󰖭, hyprctl dispatch killactive"
-              "rgb(eeee11), 14, , hyprctl dispatch fullscreen 1"
-            ];
-          };
-          hyprexpo = {
-            columns = 3;
-            gap_size = 4;
-            bg_col = "rgb(000000)";
+            #fix_mouse = true;
           };
         };
       };
