e50db25a3da0322788927831bdcda3e19a30a469
simonoscr <13078509-simonoscr@users.noreply.gitlab.com>
2025-03-01 09:46:32 +0100

    fix(gaming): update sc, and config little helper

--- hosts/desktop/gaming/ananicy-cpp.nix
+++ hosts/desktop/gaming/ananicy-cpp.nix
@@ -1,8 +1,16 @@
 { pkgs, ... }:
 {
+  # sched-ext needs to be disabled for this to work properly
   services.ananicy = {
     enable = false;
-    package = pkgs.ananicy-cpp.override { withBpf = false; };
-    rulesProvider = pkgs.ananicy-rules-cachyos_git;
+    package = pkgs.ananicy-cpp;
+    #package = pkgs.ananicy-cpp.override { withBpf = false; };
+    rulesProvider = pkgs.ananicy-rules-cachyos;
+    extraRules = [
+      {
+        "name" = "gamescope";
+        "nice" = -20;
+      }
+    ];
   };
 }
