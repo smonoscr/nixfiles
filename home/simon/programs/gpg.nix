1f917537085676323fe959b0e90b10a1ed28b75d
smonoscr <smonoscr@noreply.codeberg.org>
2025-06-02 20:52:45 +0200

    update gpg

--- home/simon/programs/gpg.nix
+++ home/simon/programs/gpg.nix
@@ -48,10 +48,10 @@ in
     };
     services.gpg-agent = {
       enable = true;
+      pinentry.package = pkgs.pinentry-curses;
       # https://github.com/drduh/config/blob/master/gpg-agent.conf
       defaultCacheTtl = 60;
       maxCacheTtl = 120;
-      pinentryPackage = pkgs.pinentry-curses;
       extraConfig = ''
         ttyname $GPG_TTY
       '';
