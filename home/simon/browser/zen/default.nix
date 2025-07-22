d327a49f3bd0dfda8633ecc0e91e96690919dab3
smonoscr <smonoscr@noreply.codeberg.org>
2025-07-22 22:19:09 +0200

    zen: switch to community flake

--- home/simon/browser/zen/default.nix
+++ home/simon/browser/zen/default.nix
@@ -15,9 +15,35 @@ in
     module.browser.zen-browser.enable = mkEnableOption "Enables zen-browser";
   };
 
+  imports = [
+    inputs.zen-browser.homeModules.beta
+  ];
+
   config = mkIf cfg.enable {
-    home.packages = [
-      inputs.zen-browser.packages.${pkgs.system}.default
-    ];
+    programs.zen-browser = {
+      enable = true;
+      nativeMessagingHosts = [ pkgs.firefoxpwa ];
+      policies = {
+        AutofillAddressesEnabled = false;
+        AutoFillCreditCardEnabled = false;
+        DisableAppUpdate = true;
+        DisableFeedbackCommands = true;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DisableProfileImport = true;
+        DisableSetDesktopBackground = true;
+        DontCheckDefaultBrowser = true;
+        NoDefaultBookmarks = true;
+        NewTabPage = true;
+        OfferToSaveLogins = false;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+      };
+    };
   };
 }
