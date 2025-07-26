0c45901144e86479f4183626539ff50d2ad35efb
osscar <osscar@noreply.codeberg.org>
2025-07-26 14:18:02 +0200

    Adjust gaming configurations for GameMode, Gamescope, and Steam
    Increase GameMode renice value from 10 to 15 for better process priority
    Disable Gamescope system-wide to prevent conflicts with Steam sessions
    Enable Steam firewall exceptions, remote play, and Gamescope integration

--- hosts/desktop/gaming/gamescope.nix
+++ hosts/desktop/gaming/gamescope.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 {
   programs.gamescope = {
-    enable = true;
+    enable = false; # this somehow prevents fucking gamescope to run when steam is enabled. i need to add the gamescopessions of steam to use nested gamescope
     package = pkgs.gamescope_git;
-    capSysNice = false; # FIXME https://discourse.nixos.org/t/unable-to-activate-gamescope-capsysnice-option/37843
-    args = [ ];
+    capSysNice = true; # FIXME? https://discourse.nixos.org/t/unable-to-activate-gamescope-capsysnice-option/37843
+    args = [
+      "--rt"
+      "--expose-wayland"
+    ];
   };
 }
