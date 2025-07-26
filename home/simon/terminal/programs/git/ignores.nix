fd3da2cf62ee4d11b6cb92d57bb149da325b59d9
osscar <osscar@noreply.codeberg.org>
2025-07-26 14:22:21 +0200

    Update Git ignore patterns
    Add .cache/, .idea/, *.elc and auto-save-list patterns Remove results
    directory from ignored paths

--- home/simon/terminal/programs/git/ignores.nix
+++ home/simon/terminal/programs/git/ignores.nix
@@ -1,15 +1,17 @@
 _: {
   programs.git = {
     ignores = [
+      ".cache/"
       ".DS_Store"
+      ".idea/"
       "*.swp"
+      "*.elc"
+      "auto-save-list"
       "Thumbs.db"
-      ".idea"
       ".vscode"
       ".vscodium"
       ".direnv"
       "pre-commit-config.yaml"
-      "results"
     ];
   };
 }
