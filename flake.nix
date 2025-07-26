ef01e4db42eac8d437c67b58c32a5627a1b14a25
smonoscr <smonoscr@noreply.codeberg.org>
2025-07-26 14:23:02 +0200

    Update example nixpkgs URL in comment

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # default
     #nixpkgs.url = "github:NixOS/nixpkgs/0196c0175e9191c474c26ab5548db27ef5d34b05"; # default
-    #nixpkgs-private.url = "github:smonoscr/nixpkgs/nixos-unstable"; # testing private
+    #nixpkgs.url = "github:smonoscr/nixpkgs/vulkanlayer-anti-lag"; # testing private
     nixpkgs-small.url = "github:NixOS/nixpkgs/nixos-unstable-small"; # faster
     #nixpkgs-git.url = "github:NixOS/nixpkgs/master"; # better not
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05"; # current stable
