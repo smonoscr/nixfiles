9875df7c1864a889ae99aabfb9fbfbcd2ee4bde0
smonoscr <smonoscr@noreply.codeberg.org>
2025-08-23 18:00:05 +0200

    boot: update kernelParams and boot option

--- hosts/desktop/security/kernel.nix
+++ hosts/desktop/security/kernel.nix
@@ -180,6 +180,15 @@
       "nowatchdog"
 
       ''acpi_osi="Windows 2020"''
+
+      # quite boot setup
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
     ];
     blacklistedKernelModules = [
       # Obscure network protocols
