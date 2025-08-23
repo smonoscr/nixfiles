ee194199d858f6c6f412421ddc8c4c5eb686a8c6
smonoscr <smonoscr@noreply.codeberg.org>
2025-08-23 11:11:31 +0200

    fish: update config

--- home/simon/terminal/shell/fish.nix
+++ home/simon/terminal/shell/fish.nix
@@ -16,6 +16,18 @@ in
   config = mkIf cfg.enable {
     programs.fish = {
       enable = true;
+
+      functions = {
+        run = ''
+          set pkgname $argv[1]
+          set appname $argv[1]
+          if test (count $argv) -gt 1
+            set appname $argv[2]
+          end
+          nix-shell -p "$pkgname" --run "$appname"
+        '';
+      };
+
       interactiveShellInit = ''
         # Disable the greeting message.
         set fish_greeting
