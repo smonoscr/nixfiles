e49490220bb422c0ad5e34345da0a69d70d73a5d
smonoscr <smonoscr@noreply.codeberg.org>
2025-09-10 21:00:00 +0200

    lib: make it possible to exclude dirs

--- lib/default.nix
+++ lib/default.nix
@@ -9,11 +9,13 @@
       allFiles = builtins.attrNames (
         lib.attrsets.filterAttrs (
           filename: _type:
-          (_type == "directory") # include directories
-          || (
-            (filename != "default.nix") # ignore default.nix
-            && (lib.strings.hasSuffix ".nix" filename) # include .nix files
-            && (!lib.any (excluded: filename == excluded) exclude) # exclude files in exclude list
+          (!lib.any (excluded: filename == excluded) exclude) # exclude files/directories in exclude list
+          && (
+            (_type == "directory") # include directories
+            || (
+              (filename != "default.nix") # ignore default.nix
+              && (lib.strings.hasSuffix ".nix" filename) # include .nix files
+            )
           )
         ) (builtins.readDir path)
       );
